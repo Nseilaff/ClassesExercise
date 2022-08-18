29545ad649313a099b962a2112c1af05a31e1d7c
Nate S <seilaffn@gmail.com>
2022-08-18 13:19:36 -0600

    First Commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassesExercise;
+
+
+Car natesCar = new Car()
+{
+    Make = "Chevy",
+    Model = "Silverado",
+    Year = 2021
+
+};
+
+Console.WriteLine($"Nates new vehicle is {natesCar.Year} {natesCar.Make} {natesCar.Model} ");
+
+var carLot = new List<Car>();
